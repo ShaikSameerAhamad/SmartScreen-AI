6e8bd2b5d9a0841a1835556c27196dfc72b50580
Sameer Ahamad S <shaiksameer5861@gmail.com>
2025-06-30 20:21:31 +0530

    Feat: Configure admin site and add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import JobDescription, Resume, AnalysisResult
+
+# Register your models here.
+admin.site.register(JobDescription)
+admin.site.register(Resume)
+admin.site.register(AnalysisResult)
